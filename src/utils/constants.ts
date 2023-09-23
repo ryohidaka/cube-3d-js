b676326d1f07d1bc1f41ad4ed8ee8f43dfc076fa
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-09-23 11:26:28 +0900

    refact: Add comments.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,15 @@
+// Default target ID, usually referring to a 3D cube.
 export const DEFAULT_TARGET_ID = "cube-3d";
+
+// Default width and height, the typical canvas dimensions.
 export const DEFAULT_WIDTH = 500;
 export const DEFAULT_HEIGHT = 500;
+
+// Default intensity of light, typically used to adjust the strength of a 3D effect.
 export const DEFAULT_INTENSITY = 0.2;
+
+// Default scroll intensity of light. Used to adjust intensity based on scroll events.
 export const DEFAULT_SCROLL_INTENSITY = 1.5;
+
+// Default swipe intensity of light. Used to adjust intensity based on swipe events.
 export const DEFAULT_SWIPE_INTENSITY = 75;
